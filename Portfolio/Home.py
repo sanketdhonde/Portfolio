8d651b0d0d0a1a570ea6d73edcb344cd7f6aef20
sanketdhonde <sanketdhonde222@gmail.com>
2024-06-25 18:57:51 +0530

    added everthing till now

--- Portfolio/Home.py
+++ Portfolio/Home.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image("image/photos.jpg")
+    st.image("image/photos.jpg", width=400)
 
 with col2:
     st.title("Sanket Dhonde")
