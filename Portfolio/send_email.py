2a7e59f198cbddfb0eaa88e20b288fda7ca26c9e
sanketdhonde <sanketdhonde222@gmail.com>
2024-06-25 18:49:30 +0530

    added the email and all the contact details

--- Portfolio/send_email.py
+++ Portfolio/send_email.py
@@ -1,11 +1,11 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "sanketdhonde222@gmail.com"
-    password = "zybj opfa ffdy ccqp"
+    password = os.getenv("PASSWORD")
     receiver = "sanketdhonde222@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
